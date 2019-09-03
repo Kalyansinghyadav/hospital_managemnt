9bbc0d53ec0075fbe56421054d0cbc3dca8d2e9d
Kalyansinghyadav <Kalyan.singh@berylsystems.com>
2019-09-03 19:21:15 +0530

    modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
     root to: "home#index"
-    get "/doctor" => "doctor#index" 
-    get "/doctor/edit/:id" => "doctor#edit"
-    get "/doctor/destroy/:id" => "doctor#destroy"
+    get "/doctors" => "doctors#index" 
+    get "/doctors/edit/:id" => "doctors#edit"
+    get "/doctors/destroy/:id" => "doctors#destroy"
+    get "/doctors/new" => "doctors#new"
  end
