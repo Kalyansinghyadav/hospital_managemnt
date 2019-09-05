1861463665ddb9ecf36af4cd668b2afb1f3a97b3
Kalyansinghyadav <Kalyan.singh@berylsystems.com>
2019-09-05 19:17:31 +0530

    create patient model

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,43 @@
+class PatientsController < ApplicationController
+
+	def new
+		@patient = Patient.new
+	end
+
+	def index
+		@patients = Patient.all
+	end
+
+	def show
+		@patient = Patient.find(params[:id])
+	end
+	def create
+		patient = Patient.new
+		patient.name = params[:patient][:name]
+		patient.age = params[:patient][:age]
+		patient.address = params[:patient][:address]
+		patient.contact = params[:patient][:contact]
+		patient.gender = params[:patient][:gender]
+		patient.save
+		redirect_to "/patients"
+	end
+
+	def destroy
+		@patient = Patient.find(params[:id])
+		@patient.destroy
+		redirect_to "/patients"
+	end
+
+	def edit
+		@patient = Patient.find(params[:id])
+	end
+
+	def update
+		@patient = Patient.find(params[:id])
+		if @patient.update(name: params[:patient][:name],age: params[:patient][:age],
+			address: params[:patient][:address], contact: params[:patient][:contact],
+			 gender: params[:patient][:gender])
+		   redirect_to "/patients"
+		  end
+	end
+end
