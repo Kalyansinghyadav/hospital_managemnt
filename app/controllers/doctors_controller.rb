6ae4a500b73a0387e83d34ba5edf83aa21783117
Kalyansinghyadav <Kalyan.singh@berylsystems.com>
2019-09-05 11:00:08 +0530

    modified files

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,13 +1,20 @@
-class DoctorController < ApplicationController
+class DoctorsController < ApplicationController
 
 	def new
 		@doctor = Doctor.new
 	end
 
-
 	def index
 		@doctors = Doctor.all
 	end
+
+	def show
+		@doctor = Doctor.find(params[:id])
+	end
+
+    def edit
+    	@doctor = Doctor.find(params[:id])
+    end
 
 	def create
 		doctor = Doctor.new
@@ -15,8 +22,24 @@ class DoctorController < ApplicationController
 		doctor.contact_no = params[:doctor][:contact_no]
 		doctor.address = params[:doctor][:address]
 		doctor.salary = params[:doctor][:salary]
+		doctor.role_id = params[:doctor][:role_id]
 		doctor.save
-		redirect_to "/doctor"
+		redirect_to "/doctors"
+	end
+	def update
+		@doctor = Doctor.find(params[:id])
+		if @doctor.update(name: params[:doctor][:name], contact_no: params[:doctor][:contact_no], address:
+			params[:doctor][:address],salary: params[:doctor][:salary])
+		    redirect_to "/doctors"
+         else
+         	render "edit"
+		end
+	end
+
+	def destroy
+		@doctor = Doctor.find(params[:id])
+		@doctor.destroy
+		redirect_to "/doctors"
 	end
 
 end
